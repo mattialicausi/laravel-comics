dff907a2ad63b8932fef08b19563062cf9f9b722
mattialicausi <licausivitomattia@gmail.com>
2022-12-15 16:42:46 +0100

    Aggiunto header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,37 @@
 <header>
-    sono l'header
+
+    <div class="my-container d-flex justify-content-between align-items-center">
+
+        <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="logo">
+
+        <div class="d-flex justify-content-between align-items-center p-4">
+
+                <a class="text-uppercase" href="#">characters</a>
+
+                <a class="text-uppercase" href="#">comics</a>
+
+                <a class="text-uppercase" href="#">movies</a>
+
+                <a class="text-uppercase" href="#">tv</a>
+
+                <a class="text-uppercase" href="#">games</a>
+
+                <a class="text-uppercase" href="#">collectibles</a>
+
+                <a class="text-uppercase" href="#">videos</a>
+
+                <a class="text-uppercase" href="#">fans</a>
+
+                <a class="text-uppercase" href="#">news</a>
+
+                <a class="text-uppercase" href="#">shop</a>
+
+        </div>
+
+        <div class="container-search">
+            <span>Search</span>
+            <span><i class="fa-solid fa-magnifying-glass"></i></span>
+        </div>
+
+    </div>
 </header>
