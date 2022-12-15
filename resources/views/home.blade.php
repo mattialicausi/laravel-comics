26527ce4679107eea5e5d76106760e84db4f15b9
mattialicausi <licausivitomattia@gmail.com>
2022-12-15 17:39:48 +0100

    Aggiunto sezione merchandising

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,9 +18,42 @@
                 </div>
             @endforeach
 
+            <div class="btn-main text-uppercase">load more</div>
 
         </div>
 
     </div>
+
+        {{-- MERCHANDISING --}}
+
+        <div class="container-merch">
+            <div class="product my-container row ">
+                <div class="col d-flex align-items-center py-3">
+                    <img src="{{Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="image product">
+                    <div class="text-uppercase px-2">digital comics</div>
+                </div>
+
+                <div class="col d-flex align-items-center py-3">
+                    <img src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="image product">
+                    <div class="text-uppercase px-2">dc merchandise</div>
+                </div>
+
+                <div class="col d-flex align-items-center py-3">
+                    <img src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="image product">
+                    <div class="text-uppercase px-2">subscription</div>
+                </div>
+
+                <div class="col d-flex align-items-center py-3>
+                    <img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="image product">
+                    <div class="text-uppercase px-2">comic shop location</div>
+                </div>
+
+                <div class="col d-flex align-items-center py-3">
+                    <img src="{{Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="image product">
+                    <div class="text-uppercase px-2">dc power visa</div>
+                </div>
+
+            </div>
+        </div>
 
 @endsection
