469106eb671554cb009d1bdc20859ad6c5da2a6d
mattialicausi <licausivitomattia@gmail.com>
2022-12-15 17:51:30 +0100

    Aggiunto classe attiva nei link in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,30 +1,30 @@
-<header>
+<header class="h-100">
 
-    <div class="my-container d-flex justify-content-between align-items-center">
+    <div class="my-container d-flex justify-content-between align-items-center h-100">
 
         <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="logo">
 
-        <div class="d-flex justify-content-between align-items-center p-4">
+        <div class="d-flex justify-content-between align-items-center p-4 h-100">
 
-                <a class="text-uppercase" href="#">characters</a>
+                <a class="text-uppercase h-100" href="#">characters</a>
 
-                <a class="text-uppercase" href="#">comics</a>
+                <a class="text-uppercase h-100 nav-link {{Route::currentRouteName() == 'comics' ? 'activeLink': ''}}" href="{{route('comics')}}">comics</a>
 
-                <a class="text-uppercase" href="#">movies</a>
+                <a class="text-uppercase h-100" href="#">movies</a>
 
-                <a class="text-uppercase" href="#">tv</a>
+                <a class="text-uppercase h-100" href="#">tv</a>
 
-                <a class="text-uppercase" href="#">games</a>
+                <a class="text-uppercase h-100" href="#">games</a>
 
-                <a class="text-uppercase" href="#">collectibles</a>
+                <a class="text-uppercase h-100" href="#">collectibles</a>
 
-                <a class="text-uppercase" href="#">videos</a>
+                <a class="text-uppercase h-100" href="#">videos</a>
 
-                <a class="text-uppercase" href="#">fans</a>
+                <a class="text-uppercase h-100" href="#">fans</a>
 
-                <a class="text-uppercase" href="#">news</a>
+                <a class="text-uppercase h-100" href="#">news</a>
 
-                <a class="text-uppercase" href="#">shop</a>
+                <a class="text-uppercase h-100" href="#">shop</a>
 
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $fumetti = config('comics.fumetti');
 
-    return view('home', compact('fumetti'));
+    return view('comics', compact('fumetti'));
 
-})->name('home');
+})->name('comics');
